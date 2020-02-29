e50fabd0b16d2d9759451debbc987bc155384d31
shihjyun <shihjyun.yeo@gmail.com>
2020-02-29 23:48:44 +0800

    initial commit

---
+++ R/analysis.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(readr)
+
+
+
+# Access Daily Covid-19 Dataset -------------------------------------------
+
+Confirmed <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+Deaths <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
+Recovered <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
+
+
+
+
+
+# data wrangling ----------------------------------------------------------
+
+Confirmed <- wrangle_covid_raw(Confirmed) %>% 
+  rename("acum_confirmed" = acum_value)
+Deaths <- wrangle_covid_raw(Deaths) %>% 
+  rename("acum_deaths" = acum_value)
+Recovered <- wrangle_covid_raw(Recovered) %>% 
+  rename("acum_recovered" = acum_value)
+
+covid_all <- Confirmed %>% 
+  mutate(acum_deaths = Deaths$acum_deaths) %>% 
+  mutate(acum_recovered = Recovered$acum_recovered) %>% 
+  select(-value, -Lat, -Long)
+
+write_csv(covid_all, "data/covid_all.csv")
---
+++ R/wrangler.R
@@ -0,0 +1,13 @@
+
+wrangle_covid_raw <- function(data){
+  
+  data %>% 
+    pivot_longer(5:42, names_to = "Date") %>% 
+    mutate(Date = str_c(Date, "20")) %>% 
+    mutate(Date = as.Date(Date, format = "%m/%d/%y")) %>% 
+    group_by(`Country/Region`, `Province/State`) %>% 
+    mutate(acum_value = value + lag(value)) %>% 
+    mutate(acum_value = if_else(is.na(acum_value), value, acum_value)) %>% 
+    ungroup()
+
+}
