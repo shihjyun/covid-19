abd5d6342530878b9aa9465f622bb2d21b30e982
shihjyun <shihjyun.yeo@gmail.com>
2020-03-01 16:02:50 +0800

    fix acum method

--- R/wrangler.R
+++ R/wrangler.R
@@ -6,8 +6,7 @@ wrangle_covid_raw <- function(data){
     mutate(Date = str_c(Date, "20")) %>% 
     mutate(Date = as.Date(Date, format = "%m/%d/%y")) %>% 
     group_by(`Country/Region`, `Province/State`) %>% 
-    mutate(acum_value = value + lag(value)) %>% 
-    mutate(acum_value = if_else(is.na(acum_value), value, acum_value)) %>% 
+    mutate(acum_value = value) %>% 
     ungroup()
 
 }
